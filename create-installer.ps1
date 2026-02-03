b173e62a9761d63e9a49a277d143b5acf68634ce
iqnite <iqnitetechnologies@gmail.com>
2026-02-03 13:50:25 +0100

    Bundle installer in single EXE Fixes #2

--- create-installer.ps1
+++ create-installer.ps1
@@ -31,7 +31,7 @@ if ($help -or (-not $output -or $inputFiles.count -lt 1) -and -not $config) {
     exit
 }
 
-if (-not (Get-Module -ListAvailable -Name PS2EXE)) {
+if (-not (Get-Module -ListAvailable -Name ps2exe)) {
     Install-Module -Name PS2EXE -Scope CurrentUser -Force
 }
 
@@ -51,73 +51,37 @@ else {
 }
 
 $embeddedInstallScript = @'
-$scriptPath = $PSScriptRoot
+$certPath = Join-Path $env:TEMP "msix-no-cert.certificate.cer"
+$installerPath = Get-ChildItem -Path $env:TEMP -Filter msix-no-cert.installer.* | Select-Object -First 1 -ExpandProperty FullName
 
-$certPath = Get-ChildItem -Path $scriptPath -Filter *.cer | Select-Object -First 1 -ExpandProperty FullName
-$installerPath = Get-ChildItem -Path $scriptPath -Filter installer.* | Select-Object -First 1 -ExpandProperty FullName
+if (Test-Path $certPath) {
+    $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certPath)
 
-$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certPath)
-
-$store = New-Object System.Security.Cryptography.X509Certificates.X509Store("TrustedPeople", "LocalMachine")
-$store.Open("ReadWrite")
-$store.Add($cert)
-$store.Close()
+    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("TrustedPeople", "LocalMachine")
+    $store.Open("ReadWrite")
+    $store.Add($cert)
+    $store.Close()
+}
 
 Start-Process -FilePath $installerPath
 '@
 
-$workingDir = Get-Location
-$tempDir = Join-Path $env:TEMP "msix-no-cert-installer"
-
-$installSourcePath = Join-Path $env:TEMP "msix-no-cert.install.ps1"
-$installOutputPath = Join-Path $env:TEMP "msix-no-cert.install.exe"
-Set-Content -Path $installSourcePath -Value $embeddedInstallScript -Encoding UTF8
-
-Invoke-PS2EXE `
-    -InputFile $installSourcePath `
-    -OutputFile $installOutputPath `
-    -IconFile $configObj.icon `
-    -title $configObj.title `
-    -description $configObj.description `
-    -version $configObj.version `
-    -NoConsole `
-    -RequireAdmin `
-    -verbose
-    
-$7zipPath = "${env:ProgramFiles}\7-Zip\7z.exe"
-
-if (-not (Test-Path $7zipPath)) {
-    Write-Host "7-Zip not found. Please install 7-Zip to continue." -BackgroundColor Red
-    exit
-}
-
 for ($i = 0; $i -lt $configObj.input.count; $i++) {
-    Remove-Item $tempDir -Recurse -Force -ErrorAction SilentlyContinue
-    New-Item -ItemType Directory -Path $tempDir -Force
-    Copy-Item $installOutputPath (Join-Path $tempDir "install.exe") -Force
-    if ($configObj.cert) {
-        Copy-Item $configObj.cert (Join-Path $tempDir "certificate.cer") -Force
-    }
-
-    $zipPath = Join-Path $configObj.output "$(Split-Path $configObj.input[$i] -Leaf).zip"
-    Write-Host "Creating archive for $($configObj.input[$i]) at $zipPath" -ForegroundColor Cyan
-    $fileName = Split-Path $configObj.input[$i] -Leaf
-    $extension = [System.IO.Path]::GetExtension($fileName)
+    $installSourcePath = Join-Path $env:TEMP "msix-no-cert.setup.ps1"
+    Set-Content -Path $installSourcePath -Value $embeddedInstallScript -Encoding UTF8
     
-    Copy-Item $configObj.input[$i] (Join-Path $tempDir "installer$extension") -Force
-    Remove-Item $zipPath -ErrorAction SilentlyContinue
-
-    Push-Location $tempDir
-    try {
-        & $7zipPath a -tzip (Join-Path $workingDir $zipPath) "*"
-    }
-    finally {
-        Pop-Location
-    }
-
-    Write-Host "Archive created at $zipPath" -ForegroundColor Blue
+    $extension = [System.IO.Path]::GetExtension($configObj.input[$i])
+    Invoke-PS2EXE `
+        -InputFile $installSourcePath `
+        -OutputFile (Join-Path $configObj.output "$(Split-Path $configObj.input[$i] -Leaf).exe") `
+        -IconFile $configObj.icon `
+        -title $configObj.title `
+        -description $configObj.description `
+        -version $configObj.version `
+        -embedFiles @{"%TEMP%\msix-no-cert.certificate.cer" = $configObj.cert; "%TEMP%\msix-no-cert.installer$extension" = $configObj.input[$i] } `
+        -NoConsole `
+        -RequireAdmin `
+        -verbose
 }
-
-Remove-Item $installSourcePath -ErrorAction SilentlyContinue
 
 Write-Host "$($configObj.input.count) installers created." -ForegroundColor Green
